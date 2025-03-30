2adbb02daba709c20f12ff44aeb570f1fe241b2b
unknown <thiagostayner@gmail.com>
2025-03-30 15:18:41 -0300

    alteração arquivo12-tkinter

--- 2-PythonModulos/12-tkinter.py
+++ 2-PythonModulos/12-tkinter.py
@@ -18,6 +18,14 @@ frase_lab = tk.Label(frame, text='Frase')
 frase_lab.pack(fill='x', expand=True)
 
 frase_input = tk.Entry(frame)
-frase
+frase_input.pack(fill='x', expand=True)
+
+# 5 - Função para alterar o texto do label
+def click():
+    label.config(text=frase_input)
+
+# 6 Adicionando botão
+botao = tk.Button(fram, text="Enviar", command=click)
+botao.pack()
 
 janela.mainloop()
